5037baacfe9c81820212fae08b2ca2e337f69bdf
moran <sharon.ky.moran@gmail.com>
2025-07-14 17:51:25 -0400

    create class based employee model

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+#create class based employee model
+class Employee(models.Model):
+    #give the model a few data points to work with later
+    emp_id = models.CharField(max_length=20)
+    emp_name = models.CharField(max_length=50)
+    designation = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.emp_name
