7b2b5e1f77a6510a76168b7f0f1635d0a313a642
moran <sharon.ky.moran@gmail.com>
2025-07-15 13:06:52 -0400

    employee detail class based view Put() is wokring

--- api/views.py
+++ api/views.py
@@ -108,3 +108,14 @@ class EmployeeDetail(APIView):
         employee = self.get_object(pk)
         serializer = EmployeeSerializer(employee)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
