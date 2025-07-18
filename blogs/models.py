840e8a81dc4967b18aaa370ed66fff04adbdcd4e
moran <sharon.ky.moran@gmail.com>
2025-07-18 17:27:36 -0400

    create blog and comment model, connect them by foreign key

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+#create Blog Model
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=100)
+    blog_body = models.TextField()
+
+    def __str__(self):
+        return self.blog_title
+    
+#create Comment Model
+class Comment(models.Model):
+    #create realtionship between blog and comments as each blog can have many comments
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE) #if blog is deleted then comment gets deleted
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.comment
