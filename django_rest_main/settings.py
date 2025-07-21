d1e206cebefe5059db4b2a3ffb11cf94d39b99bb
moran <sharon.ky.moran@gmail.com>
2025-07-20 20:45:06 -0400

    implement global pagination for generics andviewsets with LimitOffsetPagination

--- django_rest_main/settings.py
+++ django_rest_main/settings.py
@@ -127,7 +127,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #this is how we set up global pagination using PageNumberPagination
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 2,
+# }
+
+#this is how we set up global pagination using LimitOffsetPagination
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 2,
 }
