d17b0dd9df27d6fdaae2c9e163e9bf4f456ca22f
moran <sharon.ky.moran@gmail.com>
2025-07-20 20:41:12 -0400

    implement global pagination for our generics and viewset views with page size of 2

--- django_rest_main/settings.py
+++ django_rest_main/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#this is how we set up global pagination using PageNumberPagination
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
+}
