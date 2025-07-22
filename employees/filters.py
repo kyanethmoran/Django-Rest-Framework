e0510e6d89a994564b93cdfe5daca74d44256034
moran <sharon.ky.moran@gmail.com>
2025-07-21 22:02:32 -0400

    filter with a range using EMP_ID

--- employees/filters.py
+++ employees/filters.py
@@ -7,8 +7,21 @@ class EmployeeFilter(django_filters.FilterSet):
     designation = django_filters.CharFilter(field_name='designation', lookup_expr= 'iexact')
     emp_name = django_filters.CharFilter(field_name= 'emp_name', lookup_expr= 'iexact')
     id = django_filters.NumberFilter(field_name= 'id')
+    #to be able to search within the range of emp_id we need to modify how id_min & id_max work
+    #pass a custom function to the method param that will allow us to isolate the numbers only
+    #within emp_id, label parameter will change the label about the input field in the filter popup
+    id_min = django_filters.CharFilter(method= 'filter_by_emp_id_range', label = "From EMP ID")
+    id_max = django_filters.CharFilter(method= 'filter_by_emp_id_range', label = 'To EMP ID')
     
 
     class Meta:
         model = Employee
-        fields = ['designation','emp_name', 'id']
+        fields = ['designation','emp_name', 'id', 'emp_id', 'id_min', 'id_max']
+
+    #custom function to isolate the emp_id numbers to be able to filter a range of them
+    def filter_by_emp_id_range(self, queryset, name, value):
+        if name == 'id_min':
+            return queryset.filter(emp_id__gte = value)
+        elif name == 'id_max':
+            return queryset.filter(emp_id__lte = value)
+        return queryset
