ffbcb0ccd0a1ca040ead745912155acc784d93a7
moran <sharon.ky.moran@gmail.com>
2025-07-21 21:33:32 -0400

    error fix: spelling of parameter

--- employees/filters.py
+++ employees/filters.py
@@ -4,7 +4,7 @@ from .models import Employee
 class EmployeeFilter(django_filters.FilterSet):
     #the field name is the model field that we want to filter by, and iexact is how you make the filter
     #case insensitive (good for lower or upper case format of the desired lookup field)
-    designation = django_filters.CharFilter(field_name='designation', lookup_expression= 'iexact')
+    designation = django_filters.CharFilter(field_name='designation', lookup_expr= 'iexact')
 
     class Meta:
         model = Employee
