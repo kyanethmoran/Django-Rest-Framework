748d557c6a3bb7ffb4c1d7ee5d11dfe6977af76a
moran <sharon.ky.moran@gmail.com>
2025-07-21 21:00:18 -0400

    update rest framework setting to handle filtering globally

--- django_rest_main/settings.py
+++ django_rest_main/settings.py
@@ -137,4 +137,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 2,
+    #for the global filtering
+    'DEFAULT_FILTER_BACKENDS': 'django_filters.rest_framework.DjangoFilterBackend'
 }
