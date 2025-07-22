13b7c278c52537c8bb261cd99bc728f2250b0a51
moran <sharon.ky.moran@gmail.com>
2025-07-21 21:29:46 -0400

    set up the custome employee filter for case insensitive lookup

--- employees/filters.py
+++ employees/filters.py
@@ -2,4 +2,10 @@ import django_filters
 from .models import Employee
 
 class EmployeeFilter(django_filters.FilterSet):
-    pass
+    #the field name is the model field that we want to filter by, and iexact is how you make the filter
+    #case insensitive (good for lower or upper case format of the desired lookup field)
+    designation = django_filters.CharFilter(field_name='designation', lookup_expression= 'iexact')
+
+    class Meta:
+        model = Employee
+        fields = ['designation']
