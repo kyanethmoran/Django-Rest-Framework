81dfba5a3908004336855e1603946bb89dd84419
moran <sharon.ky.moran@gmail.com>
2025-07-21 21:49:17 -0400

    update the custom filtering to search  by name and id

--- employees/filters.py
+++ employees/filters.py
@@ -5,7 +5,10 @@ class EmployeeFilter(django_filters.FilterSet):
     #the field name is the model field that we want to filter by, and iexact is how you make the filter
     #case insensitive (good for lower or upper case format of the desired lookup field)
     designation = django_filters.CharFilter(field_name='designation', lookup_expr= 'iexact')
+    emp_name = django_filters.CharFilter(field_name= 'emp_name', lookup_expr= 'iexact')
+    id = django_filters.RangeFilter(field_name= 'id')
+    
 
     class Meta:
         model = Employee
-        fields = ['designation']
+        fields = ['designation','emp_name', 'id']
