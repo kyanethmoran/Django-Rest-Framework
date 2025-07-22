010f775000b3218a89fb8d9ba385bb5ff315869c
moran <sharon.ky.moran@gmail.com>
2025-07-21 20:55:44 -0400

    update to comment for clarity

--- api/paginations.py
+++ api/paginations.py
@@ -7,7 +7,7 @@ class CustomPagination(PageNumberPagination):
     page_query_param = 'page-num'
     max_page_size = 2
 
-    #override the funtion
+    #override the funtion for how we get the pagination response
     def get_paginated_response(self, data):
         return Response({
             'next': self.get_next_link(),
